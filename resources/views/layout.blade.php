459a6f4ce6db0d336b73e7186ed71790d7524268
Yamiley <thed4ydr3amer@gmail.com>
2024-10-18 18:44:56 +0200

    Falta definir carrousel y formulario

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,17 +3,17 @@
 <html lang="es">
 
 <head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Viverium - Tu espacio de plantas</title>
-    
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <link href="{{ asset('vendor/bladewind/css/bladewind-ui.min.css') }}" rel="stylesheet" />
+
 
     <link rel="icon" href="{{ asset('vendor/viverium.ico') }}" type="image/x-icon">
-    {{-- <link rel="stylesheet" href="{{ asset('css/layout.css') }}">  --}}
-</head>
+
 
 <body>
 
@@ -22,23 +22,83 @@
         <p>Tu espacio personal para cultivar y cuidar tus plantas</p>
     </header>
 
-    <nav>
-        <ul>
-            <li><a href="{{ route('my-plants.index') }}">Mis Plantas</a></li>
-            <li><a href="{{ route('my-plants.create') }}">Añadir Planta</a></li>
-            <li><a href="#">Otra Opción</a></li>
-        </ul>
+
+
+    <nav
+        class="bg-gray-700 dark:bg-lime-500 fixed w-full z-20 top-0 start-0 border-b border-green-200 dark:border-green-600">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="vendor/viverium.ico" class="h-8" alt="Viverium Logo">
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Viverium</span>
+            </a>
+            <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                <button type="button"
+                    class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-400">Get
+                    started</button>
+                <button data-collapse-toggle="navbar-sticky" type="button"
+                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-200 dark:text-gray-400 dark:hover:bg-green-700 dark:focus:ring-green-600"
+                    aria-controls="navbar-sticky" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M1 1h15M1 7h15M1 13h15" />
+                    </svg>
+                </button>
+            </div>
+            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                <ul
+                    class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-green-100 rounded-lg bg-green-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-gray-700 dark:bg-lime-500 md:dark:bg-lime-500 dark:border-green-700">
+                    <li>
+                        <a href="/"
+                            class="block py-2 px-3 text-white bg-green-700 rounded md:bg-transparent md:text-green-700 md:p-0 md:dark:text-green-500"
+                            aria-current="page">Home</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('my-plants.index') }}"
+                            class="block py-2 px-3 text-gray-900 rounded hover:bg-green-100 md:hover:bg-transparent md:hover:text-green-700 md:p-0 md:dark:hover:text-green-500 dark:text-white dark:hover:bg-green-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-green-700">My
+                            Plants</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('my-plants.create') }}"
+                            class="block py-2 px-3 text-gray-900 rounded hover:bg-green-100 md:hover:bg-transparent md:hover:text-green-700 md:p-0 md:dark:hover:text-green-500 dark:text-white dark:hover:bg-green-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-green-700">Add
+                            Plants</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
 
+
     <main>
-        <div class="container">
-            @yield('content')
-        </div>
+
+        @yield('content')
+
     </main>
 
-    <footer>
-        <p>&copy; {{ date('Y') }} Viverium. Todos los derechos reservados.</p>
-    </footer>
+    
+
+<footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" class="hover:underline">Viverium</a>. All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+            <a href="/" class="hover:underline me-4 md:me-6">Home</a>
+        </li>
+        <li>
+            <a href="{{ route('my-plants.index') }}" class="hover:underline me-4 md:me-6">My Plants</a>
+        </li>
+        <li>
+            <a href="{{ route('my-plants.create') }}" class="hover:underline me-4 md:me-6">Add Plant</a>
+        </li>
+        <li>
+            <a href="https://github.com/Yamivc" class="hover:underline">Git Hub</a>
+        </li>
+    </ul>
+</footer>
+
+
+    <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
 
 </body>
 
--- resources/views/my-plants/index.blade.php
+++ resources/views/my-plants/index.blade.php
@@ -1,5 +1,10 @@
 @extends('layout')
 
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link href="{{ asset('vendor/bladewind/css/bladewind-ui.min.css') }}" rel="stylesheet" />
+</head>
+
 @section('content')
     <h1 class="text-2xl font-bold mb-5">List of My Plants</h1>
 
@@ -11,77 +16,48 @@
 
     <section id="grid"
         class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
-
-        <!--  cards - Starts Here 👇 -->
-        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-
-            @foreach ($my_plants as $plant)
-                
-                    <img src="{{ $plant->img }}" alt="{{ $plant->name }}" alt="MyPlant"
-                        class="h-80 w-72 object-cover rounded-t-xl" />
-                    <div class="px-4 py-3 w-72">
-                        <span class="text-gray-400 mr-3 uppercase text-xs">{{ $plant->scientific_name }}</span>
-                        <p class="text-lg font-bold text-black truncate block capitalize my-2">{{ $plant->name }}</p>
-
-                        <p class="text-md font-regular text-black my-1 mx-2">Observations:</p>
-                        <p class="text-gray-600 mt-1 text-left mx-2">{{ Str::limit($plant->observations, 360) }} </p>
-
-
-                        <form action="{{ route('my-plants.destroy', $plant->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" onclick="return confirm('Are you sure you want to delete this plant?')"
-                                class="flex items-center justify-end ">
-
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"
-                                    class="w-5 h-5 text-red-500 mx-2 my-5">
-                                    <path
-                                        d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" />
-                                </svg>
-                            </button>
-
-
-                        </form>
-
-                        <div class=" flex justify-between items-center">
-                            <a href="{{ route('my-plants.edit', $plant->id) }}" class="text-blue-500 hover:underline">Edit
-                            </a>
-                        </div>
-
-
-                    </div>
-                
-            @endforeach
-        </div>
-
-    </section>
-
-
-    
-
-    
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 p-5">
         @foreach ($my_plants as $plant)
-            <x-bladewind::card class="cursor-pointer hover:shadow-lg">
-                <div class="h-32 w-full overflow-hidden rounded">
-                    <img src="{{ $plant->img }}" alt="{{ $plant->name }}" class="w-full h-full object-cover">
-                </div>
+            <!--  cards - Starts Here 👇 -->
+            <div class="w-72 h-auto bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
 
-                <h2 class="font-bold text-lg mt-4 text-center">{{ $plant->name }} ({{ $plant->scientific_name }})</h2>
 
-                <p class="text-gray-600 mt-2 text-center">{{ Str::limit($plant->observations, 100) }}</p>
 
-                <div class="mt-4 flex justify-center space-x-2">
-                    <a href="{{ route('my-plants.edit', $plant->id) }}" class="text-blue-500 hover:underline">Edit</a>
+                <img src="{{ $plant->img }}" alt="{{ $plant->name }}" alt="MyPlant"
+                    class="h-80 w-72 object-cover rounded-t-xl" />
+                <div class="px-4 py-3 max-w-72 h-auto text-wrap">
+                    <span class="text-gray-400 mr-3 uppercase text-xs">{{ $plant->scientific_name }}</span>
+                    <p class="text-lg font-bold text-black truncate block capitalize my-2">{{ $plant->name }}</p>
+
+                    <p class="text-md font-regular text-black my-1 mx-2">Observations:</p>
+                    <p class="text-gray-600 mt-1 text-left mx-2 truncate line-clamp-3">
+                        {{ Str::limit($plant->observations, 300) }} </p>
+
 
                     <form action="{{ route('my-plants.destroy', $plant->id) }}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="text-red-500 hover:underline"
-                            onclick="return confirm('Are you sure?')">Delete</button>
+                        <button type="submit" onclick="return confirm('Are you sure you want to delete this plant?')"
+                            class="flex items-center justify-end ">
+
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"
+                                class="w-5 h-5 text-red-500 mx-2 my-5">
+                                <path
+                                    d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" />
+                            </svg>
+                        </button>
+
+
                     </form>
+
+                    <div class=" flex justify-between items-center">
+                        <a href="{{ route('my-plants.edit', $plant->id) }}" class="text-blue-500 hover:underline">Edit
+                        </a>
+                    </div>
+
+
                 </div>
-            </x-bladewind::card>
+
+
+            </div>
         @endforeach
-    </div>
-@endsection
+    </section>
